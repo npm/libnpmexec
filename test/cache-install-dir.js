500db047d8d8b22753be6cf4b353b64f0d312531
Gar <gar+gh@danger.computer>
2021-06-17 08:57:13 -0700

    BREAKING CHANGE: use new npxCache option
    feat(config): add npxCache option
    This will allow us to separate the `cache` option we pass down into
    pacote from the option we use to determine where to write npx-specific
    data.

--- test/cache-install-dir.js
+++ test/cache-install-dir.js
@@ -2,10 +2,10 @@ const t = require('tap')
 
 const cacheInstallDir = require('../lib/cache-install-dir.js')
 
-t.test('invalid cache path', t => {
+t.test('invalid npxCache path', t => {
   t.throws(
     () => cacheInstallDir({}),
-    /Must provide a valid cache path/,
+    /Must provide a valid npxCache path/,
     'should throw invalid path error'
   )
   t.end()
